57c06ed2a4a901d30d13d1e3f77ee040094ba1e4
uros.petrovic <uros.petrovic@quantox.com>
2022-09-05 15:59:57 +0200

    Request changed

--- src/services/drivers/index.ts
+++ src/services/drivers/index.ts
@@ -1,7 +1,7 @@
 import axios from "src/config/axios";
 
 async function getDrivers({ offset = "" }: { offset?: string | string[] }) {
-    const url = `/drivers.json?limit=36&${offset && `offset=${offset}`}`;
+    const url = `/drivers.json?limit=36${offset && `&offset=${offset}`}`;
     const { data } = await axios.get(url);
     return data;
 }
